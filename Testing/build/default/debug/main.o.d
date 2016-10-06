b79b2dd6f2aa6d7670780559ed8b73925da97b70
Samuel James GLEESON <sgle6040@MXLAB.ACFR.USYD.EDU.AU>
2016-10-06 12:59:06 +1100

    IR sensor raw data read from a0

---
+++ Testing/build/default/debug/main.o.d
@@ -0,0 +1,4 @@
+build/default/debug/main.o:  \
+	V:/Sam_Neill_yerMum/Testing/main.c  \
+	V:/Sam_Neill_yerMum/Testing/ConfigRegs.h  \
+
---
+++ Testing/build/default/production/main.o.d
@@ -0,0 +1,5 @@
+build/default/production/main.o:  \
+	V:/Sam_Neill_yerMum/Testing/main.c  \
+	V:/Sam_Neill_yerMum/Testing/main.h  \
+	V:/Sam_Neill_yerMum/Testing/ConfigRegs.h  \
+
