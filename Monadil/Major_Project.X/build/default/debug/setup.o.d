b1df0306f51a204ff80067521494d9f22009139b
W Marais <W Marais>
2016-10-12 14:02:17 +1100

    Updated user input

--- Monadil/Major_Project.X/build/default/debug/setup.o.d
+++
@@ -1,4 +0,0 @@
-build/default/debug/setup.o:  \
-	E:/Major_Project.X/setup.c  \
-	E:/Major_Project.X/setup.h  \
-
--- Monadil/Major_Project.X/build/default/production/movement.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/movement.o:  \
-	E:/Major_Project.X/movement.c  \
-
